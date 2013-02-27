4596dfd819d7d5a6648da17cbe7903f37659e533
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-02-27 10:02:40 +0900

    impl native

--- jni/Native.c
+++ jni/Native.c
@@ -6,6 +6,14 @@ Java_net_kaoriya_qb_unexpected_1alignment_Native_alignment1(
         JNIEnv *env, jclass clazz)
 {
     jstring retval = NULL;
-    retval = (*env)->NewStringUTF(env, "");
-    return retval;
+    int a[] = { 0, 0 };
+    int *p0, *p1;
+    char buf[256];
+
+    p0 = &a[0];
+    p1 = ((int *)((char *)p0 + 1));
+    *p1 = 0x12345678;
+    sprintf(buf, "a[0]=0x%08x a[1]=0x%08x\np0=%p p1=%p\n", a[0], a[1], p0, p1);
+
+    return (*env)->NewStringUTF(env, buf);
 }
